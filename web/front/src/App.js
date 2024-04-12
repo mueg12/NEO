018b2d5dfa25b1849a34ff834c2ee2770fb5ca8a
mueg12 <mueg12@ajou.ac.kr>
2024-04-12 20:20:49 +0900

    Remove alerts

--- web/front/src/App.js
+++ web/front/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+//import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Login from './Login';
 import UpdateTest from './UpdateTest';
 import './App.css';
-import LoginForm from './LoginForm';
+//import LoginForm from './LoginForm';
 
 function App() {
   return (    
