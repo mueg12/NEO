1cf116bc49255c1c273af371379f3015f3130b45
mueg12 <mueg12@ajou.ac.kr>
2024-04-12 20:22:24 +0900

    use .env & update gitaction

--- web/back/src/main/java/com/neo/back/BackApplication.java
+++ web/back/src/main/java/com/neo/back/BackApplication.java
@@ -2,11 +2,20 @@ package com.neo.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BackApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> {
+            // .env 파일의 키를 Spring 프로퍼티 명명 규칙에 맞게 변환
+            // 예: SPRING_DATASOURCE_URL -> spring.datasource.url
+            String propName = entry.getKey().toLowerCase().replace('_', '.');
+            System.setProperty(propName, entry.getValue());
+			System.out.println(entry.getKey() + ":" + entry.getValue());
+        });
 		SpringApplication.run(BackApplication.class, args);
 	}
 
