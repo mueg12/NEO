d7d31a6019029669dfb7098b4b9ae674ca059a5a
mueg12 <mueg12@ajou.ac.kr>
2024-04-16 00:54:03 +0900

    add env.properties

--- web/back/src/main/java/com/neo/back/BackApplication.java
+++ web/back/src/main/java/com/neo/back/BackApplication.java
@@ -2,19 +2,19 @@ package com.neo.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BackApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> {
-            // .env 파일의 키를 Spring 프로퍼티 명명 규칙에 맞게 변환
-            // 예: SPRING_DATASOURCE_URL -> spring.datasource.url
-            String propName = entry.getKey().toLowerCase().replace('_', '.');
-            System.setProperty(propName, entry.getValue());
-        });
+		// Dotenv dotenv = Dotenv.load();
+        // dotenv.entries().forEach(entry -> {
+        //     // .env 파일의 키를 Spring 프로퍼티 명명 규칙에 맞게 변환
+        //     // 예: SPRING_DATASOURCE_URL -> spring.datasource.url
+        //     String propName = entry.getKey().toLowerCase().replace('_', '.');
+        //     System.setProperty(propName, entry.getValue());
+        // });
 		SpringApplication.run(BackApplication.class, args);
 	}
 
