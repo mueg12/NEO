8acbeb3eb0e35c0d9fc771ced4c89c508cbd98b2
malenwater <malenwater@ajou.ac.kr>
2024-05-11 01:11:39 +0900

    [#14] make docker images game version
    Done
    1. make docker images minecraft version

---
+++ game-server/dokertest/control/start.sh
@@ -0,0 +1,15 @@
+timeout 5m tail -n 5 -f /control/output.txt | {
+    flag=0
+    while IFS= read -r line; do
+        # echo "$line"
+        if [[ "$line" == *"Done"* ]]; then
+            echo "startAck"
+            pkill -P $$ tail  # 현재 스크립트의 부모 프로세스로 속한 tail 프로세스를 종료
+            flag=1
+            break
+        fi
+    done
+    if [ $flag -eq 0 ]; then
+        echo "startERR"  # 2분 동안 실패한 경우
+    fi
+}
